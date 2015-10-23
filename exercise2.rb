e157b7bd7055e7155a178e27b1c447fdc61cc67b
Carter-A <carter.s.andrew@gmail.com>
2015-10-23 16:06:43 -0400

    exercise two

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+# tip on a 55 dollar meal. 12% cause im cheap.
+puts (55 * 1.12).round(2)
+
+# adding strings and an integer converted to a string
+puts 5.to_s + "r"
+
+# String interpolation
+x = 45628 * 7839
+puts "45628 multiplied by 7839 is equal to #{x}"
+
+#Boolean operators
+#(true && false) === false
+#(false && true) === false
+#!(false && false) === true
